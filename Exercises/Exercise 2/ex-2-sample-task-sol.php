15cec58a5f9780ebd098a26b0ee7960f3f0ef0d1
Martin Duhovnikov <martin.duhovnikov@externals.adidas.com>
2020-02-29 20:34:36 +0200

    Moved text files to HTML and added bonus tasks + sample solution for Exercise 2

---
+++ Exercises/Exercise 2/ex-2-sample-task-sol.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Exercise 2 - Task</title>
+  </head>
+  <body>
+		<h1>Multiplication Table in PHP</h1>
+		<?php
+			$rows = 9;
+			$cols = 9;
+			$styles = 'padding: 10px 15px; text-align: center; background-color: #d3d3d3;';
+
+			echo "<table border =\"1\" style='border-collapse: collapse;'>";
+			echo "<tr style='font-weight: bold;'>";
+				echo "<th style='$styles'>1</th>";
+				for ($col = 2; $col <= $cols; $col++) {
+				echo "<th style='$styles'>$col</th>";
+				}
+			echo "</tr>";
+			for ($row = 2; $row <= $rows; $row++) {
+				echo "<tr>";
+				echo "<th style='$styles'>$row</th>";
+				for ($col = 2; $col <= $cols; $col++) {
+					$prod = $col * $row;
+					echo "<td style='padding: 10px 15px; text-align: center;'>$prod</td>";
+				}
+				echo "</tr>";
+			}
+			echo "</table>";
+		?>
+  </body>
+</html>
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Web-14-Notes
-This is repository has some extra notes with information that could be helpful for students for successfully completing the WWW Technologies course presented at FMI, Sofia.
+This repository has some extra notes with information that could be helpful for students for successfully completing the WWW Technologies course presented at FMI, Sofia.
